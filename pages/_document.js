37b37104eb07b289996b5fd61f63a0a2e6b0c500
toan <thetoan95@gmail.com>
2020-02-21 01:10:00 +0700

    revert google verify

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,6 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-          <meta name="google-site-verification" content="0W2rDzsqRhd-XRTyDoN4VCHMhkJRuCxwJZ655AR1Zpo" />
         </Head>
         <body>
           <Main />
